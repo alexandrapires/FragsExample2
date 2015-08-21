08dc5f512a253002b0566e8df6b1705a8755b6d4
alexandrapires <alexandra.pires@africainternetgroup.com>
2015-08-21 10:15:19 +0100

    %changed on local

--- app/src/main/java/com/example/alexandrapires/fragsexample2/MainActivity.java
+++ app/src/main/java/com/example/alexandrapires/fragsexample2/MainActivity.java
@@ -2,19 +2,15 @@ package com.example.alexandrapires.fragsexample2;
 
 import android.app.FragmentManager;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 //changed on server
+// changed local
 public class MainActivity extends FragmentActivity implements ItemFragment.OnFragmentInteractionListener   {
-
-    ImageView imMenu = null;
 
     ItemFragment itemFrag;
 
