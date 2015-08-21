16ad14c158a4a8a5bc4781269edff27aa37ce017
alexandrapires <alexandra.pires@africainternetgroup.com>
2015-08-21 15:57:12 +0100

    %%% reset changes %%%

--- app/src/main/java/com/example/alexandrapires/fragsexample2/MainActivity.java
+++ app/src/main/java/com/example/alexandrapires/fragsexample2/MainActivity.java
@@ -8,10 +8,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
-//change 1 - server change
-//change 2 - local change
-//change 4 - local change to test merging
-//change 3 - server change to test merging
+
 public class MainActivity extends FragmentActivity implements ItemFragment.OnFragmentInteractionListener   {
 
     ItemFragment itemFrag;
